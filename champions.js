641330b3b10cdd76197b4b8defd2323e25bfde19
Andrew Cornejo <atcornejo3@gmail.com>
2022-09-15 12:33:52 -0700

    questioning if this is done properly

---
+++ champions.js
@@ -0,0 +1,22 @@
+export const champion = [
+
+        const champion = {
+            {
+                name: 'Aatrox',
+                type: 'Darkin',
+                area: 'Shurima',
+            },
+            {
+                name: 'Shen',
+                type: 'Ninja',
+                area: 'Ionia',
+            },
+            {
+                name: 'Garen',
+                type: 'Knight',
+                area: 'Demacia',
+            },
+
+        };
+
+    ];
